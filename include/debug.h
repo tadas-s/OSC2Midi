c7d948838f3c8f7d86db642ffcac6297a37dddb6
Tadas Sasnauskas <tadas@yoyo.lt>
2018-12-15 14:36:22 +0000

    Use uart0 for midi and uart1 for debug messages
    This ditches SoftwareSerial for which I suspect was causing crashes.
    This also ditches led blinking since built-in esp12e led is on the same pin as
    uart1 tx.

--- include/debug.h
+++ include/debug.h
@@ -1,8 +1,9 @@
 #ifndef DEBUG_H
 #define DEBUG_H
 
-#ifdef DEBUG_ESP_PORT
-#define DEBUG_MSG(...) DEBUG_ESP_PORT.printf( __VA_ARGS__ )
+#ifdef DEBUG
+#define DEBUG_ESP_PORT Serial1
+#define DEBUG_MSG(...) Serial1.printf( __VA_ARGS__ )
 #else
 #define DEBUG_MSG(...)
 #endif
